ae1a474d7a62b4f44cdc3a4b7727aa0a6c96e2c6
Abhay Raj Singh <abraj.singh743@gmail.com>
2026-04-06 21:54:47 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const connect = require('./config/database')
+
+
+app.listen(3000,async()=>{
+    console.log(`Server started at port:3000`)
+    await connect();
+    console.log("Mongo DB connected")
+
+})

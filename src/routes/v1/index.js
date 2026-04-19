772b730331032a5e64c7bc87fe622dd8074b0d58
Abhay Raj Singh <abraj.singh743@gmail.com>
2026-04-19 22:20:30 +0530

    Authenticate create comment route also

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.post('/tweets',authenticate,createTweet)
 router.post('/likes/toggle',toggleLike)
-router.post('/comments',createComment)
+router.post('/comments',authenticate,createComment)
 router.get('/tweets/:id',getTweet)
 router.post('/users/signup',signup)
 router.post('/users/login',login)
